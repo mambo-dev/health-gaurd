85b5ecd1f6573ff5450420b38b3f395e5137788a
mambo-dev <mambo.michael.22@gmail.com>
2024-02-17 20:07:16 +0300

    feat:selec:select medical history

---
+++ src/routes/onboarding/medical-history/+page.server.ts
@@ -0,0 +1,106 @@
+import { checkUserAuth } from '$lib/server/check-auth';
+import { fail, type Actions, redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { db } from '$lib/db/db';
+import { eq } from 'drizzle-orm';
+import { medicalHistory } from '$lib/db/schema';
+
+const historyOptions: string[] = [
+	'Hypertension',
+	'Diabetes',
+	'Allergies',
+	'Asthma',
+	'Migraine headaches:'
+];
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const { userId, error } = checkUserAuth(locals);
+
+	if (!userId || error) {
+		return fail(403, { error });
+	}
+
+	const findMedicalHistory = await db.query.medicalHistory.findMany({
+		where: eq(medicalHistory.userId, userId)
+	});
+
+	if (!findMedicalHistory || findMedicalHistory.length > 0) {
+		return {
+			availableHistory: [],
+			historyOptions
+		};
+	}
+
+	return {
+		availableHistory: findMedicalHistory,
+		historyOptions
+	};
+};
+
+export const actions: Actions = {
+	default: async ({ request, locals }) => {
+		const { error, userId } = checkUserAuth(locals);
+
+		if (!userId || error) {
+			return fail(403, { error });
+		}
+
+		if (!locals.user.id) {
+			throw new Error('failed to attach id on login');
+		}
+
+		const formData = await request.formData();
+
+		const selectedHistory: {
+			name: string;
+			userId: number;
+		}[] = historyOptions
+			.map((history) => {
+				const value = formData.get(`option-${history.split(' ')[0]}`);
+
+				return {
+					name: String(value),
+					userId
+				};
+			})
+			.filter((history) => history.name !== 'null');
+
+		if (selectedHistory.length <= 0) {
+			return fail(422, { error: 'you can skip this step if you dont have any medical history' });
+		}
+
+		const findMedicalHistory = await db.query.medicalHistory.findMany({
+			where: eq(medicalHistory.userId, userId)
+		});
+		if (findMedicalHistory) {
+			const savedHistory = findMedicalHistory.map((history) => {
+				return history.name;
+			});
+
+			if (savedHistory.length >= 5) {
+				return fail(422, { error: 'You have already added all available goals' });
+			}
+
+			const savedHistoryObject: {
+				[index: string]: string;
+			} = {};
+
+			savedHistory.forEach((history) => {
+				savedHistoryObject[history] = history;
+			});
+
+			await db
+				.insert(medicalHistory)
+				.values(
+					selectedHistory.filter(
+						(history) => !Object.keys(savedHistoryObject).includes(history.name)
+					)
+				);
+
+			throw redirect(303, '/onboarding/medical-history');
+		}
+		await db.insert(medicalHistory).values(selectedHistory);
+
+		throw redirect(303, '/onboarding/medical-history');
+	}
+};
--- src/routes/onboarding/medical-history/+page.svelte
+++ src/routes/onboarding/medical-history/+page.svelte
@@ -1,4 +1,96 @@
-<script lang="ts"></script>
+
+<script lang="ts" >
+    import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
+	import MultipleOptions from '$lib/components/MultipleOptions.svelte';
+	import type { PageData, PageServerData } from './$types';
 
 
-<div></div>
+
+    let loading = false
+    let error = false
+    let show = false
+    let message = ""
+
+
+    
+    export let data:PageData
+
+    let userHistory:App.FrontEndTypes["options"] = {}
+
+    let alreadySelected = data.availableHistory ? data.availableHistory?.map((history)=>{
+        return history.name
+    }) : []
+
+    data?.historyOptions?.forEach(element => {
+        userHistory[element] = {
+            option:element,
+            selected:alreadySelected?.includes(element) ?? false
+        }
+
+    });
+
+    
+
+    function submitForm(){
+        loading = true
+
+        return async (options:any)=>{
+            if(options.result.type === "failure"){
+                show = true
+                error = true 
+                message = options.result.data.error
+
+                setTimeout(()=>{
+                    show = false
+                }, 5000)
+            }
+
+            if(options.result.type === "redirect"){
+                await goto(options.result.location)
+            }
+            loading = false
+        }
+    }
+
+</script>
+
+
+<div class="max-w-xl mx-auto mt-10 bg-slate-950 rounded-md shadow-slate-900 py-4 px-2 " >
+    <div class="w-full  h-fit" >
+        <h1  class="text-slate-50 font-medium text-2xl text-center">Do you have any of this medical conditions ?  </h1>
+    </div>
+    <form  method="POST" use:enhance={submitForm} class="mt-4 w-full flex flex-col items-center gap-y-3" >
+
+        <MultipleOptions bind:options={userHistory} />
+        {#each Object.keys(userHistory).filter((history)=>{
+            const selected  = userHistory[history].selected;
+            return selected
+        }) as selectedHistory }
+            <input type="hidden" name={`option-${selectedHistory.split(" ")[0]}`}  value={selectedHistory} />
+        {/each}
+    
+        <div class="flex items-center justify-between gap-x-3 w-full mt-4 " >
+        
+            <button  disabled={loading} class=" flex-1 disabled:bg-opacity-50 w-full bg-blue-600 text-white font-medium py-2 px-4 rounded-full"    >
+            { loading ? "loading..." : "Save condition"}
+            </button>
+
+            <button on:click={async ()=>{
+                await goto("/onboarding/meal-preferences")
+            }} 
+            type="button"
+            class="hover:underline hover:bg-slate-50 hover:text-slate-900 text-white font-medium py-2 px-4 rounded-full"
+            >
+            Skip
+            </button>
+        </div>
+
+    </form>
+
+    {#if error && show}
+    <div  class="w-3/4 mx-auto mt-4 rounded-full py-1 flex items-center justify-center px-4 bg-red-500 text-sm text-slate-50 font-semibold">
+        <p>{message}</p>
+    </div>
+{/if}
+</div>
